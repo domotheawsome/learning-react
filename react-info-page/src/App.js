26d6daff7f2eb618731d741ed4e2f202258e2910
domotheawsome <cblappers@gmail.com>
2022-03-02 11:45:52 -0800

    new app

---
+++ react-info-page/src/App.js
@@ -0,0 +1,14 @@
+import './styles.css';
+import Header from './components/Header.js';
+import MainContent from './components/MainContent.js';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-info-page/src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <div>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainContent;

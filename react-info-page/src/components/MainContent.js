0363ea353cf54171fb291b121ef4434ef3a740c4
domotheawsome <cblappers@gmail.com>
2022-03-04 18:36:53 -0800

    personal website project

--- react-info-page/src/components/MainContent.js
+++ react-info-page/src/components/MainContent.js
@@ -7,6 +7,9 @@ function MainContent() {
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </div>
     )
